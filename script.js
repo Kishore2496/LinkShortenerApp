43ea6bf90996dcd01a21c526660548b587a3a5f8
Kishore Kumar S <kishore.kumar.s@oracle.com>
2022-01-04 15:59:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+var accessToeken = "d2bb61ca8d14b04e57f4a90a6c21a76b5457c13d";
+var baseURL = "https://api-ssl.bitly.com/v4";
+var shortURL;
+
+//navigator.clipboard.readText("hello");
+
+async function shorten() {
+  var URL = document.getElementById("shorten-url").value;
+
+  var group_info = await getGroupID();
+  var group_guid = group_info.groups[0].guid;
+
+  var shortenURL = baseURL + "/shorten";
+  var response = await fetch(shortenURL, {
+    method: "POST",
+    headers: {
+      authorization: "Bearer " + accessToeken,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      long_url: URL,
+      domain: "bit.ly",
+      group_guid: group_guid,
+    }),
+  });
+
+  const results = await response.json();
+  localStorage.setItem("shortened-url", results.link);
+  document.getElementById("final-url").innerText =
+    "Bit.ly URL : " + results.link;
+}
+
+async function getGroupID() {
+  const groupsURL = baseURL + "/groups";
+  let results = await fetch(groupsURL, {
+    method: "GET",
+    headers: {
+      authorization: `Bearer ${accessToeken}`,
+    },
+  });
+  return await results.json();
+}
+
+function copyToClip() {
+  navigator.clipboard.writeText(localStorage.getItem("shortened-url"));
+}
